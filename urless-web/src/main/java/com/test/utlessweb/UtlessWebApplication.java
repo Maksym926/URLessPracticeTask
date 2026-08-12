c284690e8fe61df6c1cd19612e9ef64a33faadbb
Maksym926 <maxchechotkin@gmail.com>
2026-08-12 16:33:50 +0300

    Create initial project structure

---
+++ urless-web/src/main/java/com/test/utlessweb/UtlessWebApplication.java
@@ -0,0 +1,13 @@
+package com.test.utlessweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtlessWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtlessWebApplication.class, args);
+    }
+
+}
---
+++ urless-web/src/test/java/com/test/utlessweb/controller/ShortenerControllerTest.java
@@ -0,0 +1,7 @@
+package com.test.utlessweb.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShortenerControllerTest {
+}
