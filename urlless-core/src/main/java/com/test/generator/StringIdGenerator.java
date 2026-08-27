b21386d36cd8f45e96d922963aea3d8182a56eed
Maksym926 <maxchechotkin@gmail.com>
2026-08-27 15:35:07 +0300

    StringIdGenerator: refactor the generate method

--- urlless-core/src/main/java/com/test/generator/StringIdGenerator.java
+++ urlless-core/src/main/java/com/test/generator/StringIdGenerator.java
@@ -2,39 +2,33 @@ package com.test.generator;
 
 import com.test.exceptions.FailedToCreateCollectionException;
 
+import java.security.SecureRandom;
 import java.util.Random;
 import java.util.Set;
 
 public class StringIdGenerator implements IdGenerator {
 
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int LENGTH = 5;
+    private static final int MAX_ATTEMPTS = 10;
+    private final Random random = new SecureRandom();
+
     @Override
     public String generate(String url, Set<String> collisions) {
-        boolean collision = false;
-        StringBuilder res = new StringBuilder();
-        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        Random random = new Random();
-        for (int i = 0; i < 5; i++) {
-            int index = random.nextInt(chars.length());
-            res.append(chars.charAt(index));
+        for (int attempt = 0; attempt < MAX_ATTEMPTS; attempt++) {
+            String candidate = randomCode();
+            if (!collisions.contains(candidate)) {
+                return candidate;
+            }
         }
-
-        do{
-            if(collision){
-               int randomPosition = random.nextInt(res.length());
-               int randomChar = random.nextInt(chars.length());
-               res.setCharAt(randomPosition, chars.charAt(randomChar));
-            }
-
-            if(collisions.contains(res)){
-                collision = true;
-            }
-            else{
-                return res.toString();
-            }
-
-        }while (collision);
-
         throw new FailedToCreateCollectionException();
+    }
 
+    private String randomCode() {
+        StringBuilder sb = new StringBuilder(LENGTH);
+        for (int i = 0; i < LENGTH; i++) {
+            sb.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+        }
+        return sb.toString();
     }
 }
